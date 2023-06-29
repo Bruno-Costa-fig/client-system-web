524b26a0569898ca9e570817c486a5dd56245293
Bruno-Costa-fig <brunocosta.fig@gmail.com>
2023-06-28 23:02:12 -0300

    feat: criação ranking cidades

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -4,6 +4,7 @@ import ProfileIcon from "../../components/icons/ProfileIcon"
 import MapsIcon from "../../components/icons/MapsIcon"
 import CpuIcon from "../../components/icons/CpuIcon"
 import BuildingIcon from "../../components/icons/BuildingIcon"
+import RankingCidades from "../../components/RankingCidades";
 
 function Dashboard() {
   return ( 
@@ -22,12 +23,12 @@ function Dashboard() {
           <BuildingIcon size={28} />
         </CardItem>
       </div>
-      <div className="w-full mt-5 min-h-full bg-red-200 lg:grid lg:gap-3 lg:gap-4 lg:grid-cols-4 place-content-start">
-        <div className="bg-green-200 min-h-max col-start-1 col-end-4">
+      <div className="w-full mt-5 min-h-full lg:grid lg:gap-3 lg:gap-4 lg:grid-cols-4 place-content-start">
+        <div className="min-h-max col-start-1 col-end-4">
           <h3>card</h3>
         </div>
-        <div className="bg-green-200 min-h-full">
-
+        <div className="min-h-full">
+          <RankingCidades />
         </div>
       </div>
     </ContainerWrapper>
---
+++ src/components/RankingCidades/index.jsx
@@ -0,0 +1,55 @@
+function RankingCidades() {
+
+  const cidadesList = [
+    {
+      name: "Pereiro",
+      amount: 120334
+    },
+    {
+      name: "São Miguel",
+      amount: 114423
+    },
+    {
+      name: "Jaguaribe",
+      amount: 112781
+    },
+    {
+      name: "Icó",
+      amount: 109988
+    },
+    {
+      name: "Iguatu",
+      amount: 107326
+    },
+    {
+      name: "Cedro",
+      amount: 102454
+    },
+    {
+      name: "Lavras da Mangabeira",
+      amount: 90760
+    },
+  ]
+
+  return (
+    <div className="rounded bg-white shadow-md p-10">
+      <h4 className="mb-10">Ranking de cidades</h4>
+      <div>
+        <div className="flex justify-between mb-5">
+          <span className="font-bold">Cidade</span>
+          <span className="font-bold">Montante</span>
+        </div>
+        <ul>
+          {cidadesList.map((item) => (
+            <div className="w-full flex justify-between py-5 border-t-2 border-solid border-slate-100">
+              <li>{item.name}</li>
+              <li className="whitespace-nowrap">{item.amount.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</li>
+            </div>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default RankingCidades;
