782d7c28a470c40c7805105e2521dc81c8f84e0d
Bruno Costa <62752426+Bruno-Costa-fig@users.noreply.github.com>
2024-11-17 16:38:30 -0300

    feat: adição link api publicada

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8081'
+  // baseURL: 'http://localhost:8081'
+  baseURL: 'https://client-system-api.vercel.app'
 })
 
 const handleGet = async (endpoint) => {
